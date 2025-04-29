5b1d93c3b218aa2a73229c8c9fb041b45dc09b77
naimmohamed <naimmohamed848@gmail.com>
2025-04-29 16:14:32 +0100

    First test

---
+++ app/src/main/java/com/example/sensor/ui/accelerometer/AccelerometerFragment.java
@@ -0,0 +1,71 @@
+package com.example.sensor.ui.accelerometer;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.sensor.R;
+
+public class AccelerometerFragment extends Fragment implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private TextView xValue, yValue, zValue;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_accelerometer, container, false);
+        xValue = view.findViewById(R.id.x_value);
+        yValue = view.findViewById(R.id.y_value);
+        zValue = view.findViewById(R.id.z_value);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xValue.setText(String.format("X: %.2f", event.values[0]));
+            yValue.setText(String.format("Y: %.2f", event.values[1]));
+            zValue.setText(String.format("Z: %.2f", event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Non utilisé
+    }
+}
